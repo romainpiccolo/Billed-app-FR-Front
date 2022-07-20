2a8d44b96d60a2ba7b2fe4e469571f7bbeba14bf
Romain <piccolo.rom@gmail.com>
2022-07-20 16:38:25 +0200

    [Ajout de tests unitaires et d'intégration] - Add expect in Bills first test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -25,7 +25,9 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
+
       //to-do write expect expression
+      expect(windowIcon.classList[0]).toEqual('active-icon')
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
