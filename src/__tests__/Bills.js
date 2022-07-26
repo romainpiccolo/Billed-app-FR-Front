aa5bfbec8295472847dca6f63b760eb103083679
Romain <piccolo.rom@gmail.com>
2022-07-26 17:00:00 +0200

    [Ajout de tests unitaires et d'intégration] Integration tests GET Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,25 +4,38 @@
 
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
+import Bill, { getBills } from "../containers/Bills"
+
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
 
+jest.mock("../app/store", () => mockStore)
+
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
+    beforeAll(() => {
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+            type: 'Employee'
+        }))
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        router()
+        window.onNavigate(ROUTES_PATH.Bills)
+    })
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
+    test("fetches bills from mock API GET", async () => {
+        await waitFor(() => screen.getByText("Mes notes de frais"))
+        expect(screen.getByTestId("btn-new-bill")).toBeTruthy()
+    })
+
+    test("Then bill icon in vertical layout should be highlighted", async () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
 
@@ -38,4 +51,51 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+
+    describe("When an error occurs on API", () => {
+        beforeEach(() => {
+            jest.spyOn(mockStore, "bills")
+            Object.defineProperty(
+                window,
+                'localStorage',
+                { value: localStorageMock }
+            )
+            window.localStorage.setItem('user', JSON.stringify({
+                type: 'Employee'
+            }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.appendChild(root)
+            router()
+        })
+        test("fetches bills from an API and fails with 404 message error", async () => {
+            mockStore.bills.mockImplementationOnce(() => {
+                return {
+                list : () =>  {
+                    return Promise.reject(new Error("Erreur 404"))
+                }
+                }})
+            window.onNavigate(ROUTES_PATH.Bills)
+            await new Promise(process.nextTick);
+            const message = await screen.getByText(/Erreur 404/)
+            expect(message).toBeTruthy()
+        })
+    
+        test("fetches messages from an API and fails with 500 message error", async () => {
+            mockStore.bills.mockImplementationOnce(() => {
+                return {
+                list : () =>  {
+                    return Promise.reject(new Error("Erreur 500"))
+                }
+                }})
+        
+            window.onNavigate(ROUTES_PATH.Bills)
+            await new Promise(process.nextTick);
+            const message = await screen.getByText(/Erreur 500/)
+            expect(message).toBeTruthy()
+        })
+    })
+      
+
+
 })
